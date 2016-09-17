a2f493989e4d09bf0fc7aa331909341bd6889d82
Eric Sakshaug <Eric.Sakshaug11@gmail.com>
2016-09-17 18:17:27 -0400

    Added comments to memory

--- Components/Memory.v
+++ Components/Memory.v
@@ -6,14 +6,17 @@ module Reg16(reset, CLK, D, Q);
    
 
   // I/O Declarations
-  input reset;
-  input CLK;
-  input  [15:0] D;
-  output [15:0] Q;
-  reg    [15:0] Q;
+  input RES; //The reset bit. If this is high, all bits go to 0.
+  input CLK; //Clock
+  input  [15:0] D; //Creates one bus of size 16 to address the register.  
+  output [15:0] Q; //Creates one bus of size 16 for output from the register.
+
+  //This is the actual memory unit that powers the whole thing. There is one set
+  //of 16 registers, as denoted by [15:0]. 
+  reg    [15:0] Q; 
 
   always @ (posedge CLK)
-    if(reset)
+    if(RES)
       Q=0
     else
       Q=D
